3fe2bd9fcae222d276d613d5403447f62f0d9b27
Your Name <byives21@gmail.com>
2018-12-18 19:45:27 -0800

    sql added

---
+++ hostel management/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace hostel_management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.login = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.login);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.password);
+            this.groupBox1.Controls.Add(this.username);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(308, 66);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(322, 206);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(145, 153);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(120, 37);
+            this.login.TabIndex = 4;
+            this.login.Text = "login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "password";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(106, 93);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(148, 20);
+            this.password.TabIndex = 2;
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(106, 47);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(148, 20);
+            this.username.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ hostel management/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+
+namespace hostel_management
+{
+    public partial class Form1 : Form
+    {
+        private string connectionstring;
+        private MySqlConnection connection;
+        
+         
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionstring = "server=localhost;database=hosteldb;Uid=root;pwd=0787879540";
+            connection = new MySqlConnection(connectionstring);
+            connection.Open();
+        }
+
+            private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        
+          
+
+        private void login_Click_1(object sender, EventArgs e)
+        {
+            var usernameText = username.Text;
+            var passwordText = password.Text;
+
+            var selectCommand = new MySqlCommand();
+            selectCommand.CommandText = "select * from users where username=@username AND password =@password";
+            selectCommand.Parameters.AddWithValue("@username",usernameText);
+            selectCommand.Parameters.AddWithValue("@password",passwordText);
+
+            selectCommand.Connection = connection;
+            MySqlDataReader dataReader = selectCommand.ExecuteReader();
+            if (dataReader.Read())
+            {
+                MessageBox.Show("login successful");
+            }
+
+            else
+            {
+                MessageBox.Show("Invalid Username or password");
+            }
+
+
+
+
+        }
+
+        
+
+
+        }
+    }
+
